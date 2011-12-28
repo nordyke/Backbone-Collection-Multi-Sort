c3cec27b15d0bf78b61d2f05b425028032bd3685
Aaron Nordyke <aaron.nordyke@gmail.com>
2011-12-28 13:02:54 -0600

    changed readme

---
+++ js/spec/multi-sort.collection.spec.js
@@ -0,0 +1,69 @@
+describe("Multi-sort collection",function(){
+	var models;
+			
+	beforeEach(function(){
+		models = new MultiSortCollection;
+		//models.add(new Backbone.Model({name : "Charlie",number: 5}));
+		//models.add(new Backbone.Model({name : "Billy", number: 1}));
+		//models.add(new Backbone.Model({name : "Albert",number: 1}));
+		models.add([
+			new Backbone.Model({name : "Charlie",number: 5}),
+			new Backbone.Model({name : "Billy", number: 1}),
+			new Backbone.Model({name : "Albert",number: 1})
+		]);
+	});
+	
+	describe("sort",function(){
+		
+		it("sorts according to attr",function(){
+			models.sortBy("name");
+			expect(models.pluck("name")).toEqual(["Albert","Billy","Charlie"]);
+		});
+	
+		it("sorts by two attributes",function(){
+			models.add(new Backbone.Model({name : "Charlie",number:0}));
+			models.sortBy("name","number");
+			expect(models.pluck("name")).toEqual(["Albert","Billy","Charlie","Charlie"]);
+			expect(models.pluck("number")).toEqual([1,1,0,5]);
+		});
+		
+		it("stores attributes",function(){
+			models.sortBy("name","number");
+			expect(models._sortAttributes).toEqual(["name","number"]);
+		});
+/*
+		it("resorts when order is added",function(){
+			models.sortBy("name","number");
+			expect(models.pluck("name")	).toEqual(["Albert","Billy","Charlie"]);
+			models.add({name : "Aaron",number:0});
+			expect(models.pluck("name")).toEqual(["Aaron","Albert","Billy","Charlie"]);
+		});
+*/
+	});
+	
+	describe("sortedIndex",function(){
+		
+		it("gets last index for new model when collection is not sorted",function(){
+			var model = new Backbone.Model({name : "Bart",number:0});
+			expect(models.sortedIndex(model)).toEqual(3);
+		});
+
+		it("gets index where new model should be inserted, pt 1",function(){
+			var model = new Backbone.Model({name : "Bart",number:0});
+			models.sortBy("name","number");
+			expect(models.sortedIndex(model)).toEqual(1);
+		});
+		
+		it("gets index where new model should be inserted, pt 2",function(){
+			var model = new Backbone.Model({name : "Billy",number:2});
+			models.sortBy("name","number");
+			expect(models.sortedIndex(model)).toEqual(2);
+		});
+		
+		it("gets index where new model should be inserted, pt 3",function(){
+			var model = new Backbone.Model({name : "Charlie",number:1});
+			models.sortBy("name","number");
+			expect(models.sortedIndex(model)).toEqual(2);
+		});
+	});
+});
---
+++ js/src/collections/multi-sort.collection.js
@@ -0,0 +1,114 @@
+var MultiSortCollection = Backbone.Collection.extend({
+	
+	_sorted : false,
+	
+	/**
+	 * Sort by supplied attributes.  First param is sorted first, and
+	 * last final is final subsort
+	 * @param {String} sortAttributes
+	 */
+	sortBy : function(sortAttributes){
+		var attributes = arguments;
+		if(arguments.length){
+			this._sortAttributes = arguments;
+		}
+		this.models = this._sortBy(this.models,this._sortAttributes);
+		this._sorted = true;
+	},
+	
+		/**
+	 * Returns where model would be placed into sorted collection.
+	 * Do NOT call on unsorted collection.
+	 * @param {Object} model 
+	 * @return {Integer} index where model would be inserted into sorted
+	 * 									 collection. Returns end of array, if unsorted.
+	 */
+	sortedIndex : function(model){
+		var index;
+		//return end of un-sorted arrays
+		if(!this._sorted){
+			return this.models.length;
+		}
+		return this._sortedIndex(model,this.models,this._sortAttributes);
+	},
+	
+		/**
+	 * Recursive sortedIndex
+	 * Each recursion finds sortedIndex of subset of models
+	 * that correspond to each sortattribute.
+	 * Then sum of indexes from recursions are returned
+	 */
+	_sortedIndex : function(model,models,attributes){
+		var that = this,
+				first,
+				firstIndex,
+				attr;
+		//base case #1:  no more attributes
+		if(!attributes.length){
+			return 0;
+		}
+		attr = attributes[0];
+		//get first model with identical attribute value
+		first = _(models).find(function(m){
+			return m.get(attr) === model.get(attr);
+		});
+		//base case #2:  no other model with identical attr value
+		//so just return the index where it should be inserted
+		if(_.isEmpty(first)){
+			return _(models).sortedIndex(model,function(m){
+				return m.get(attr);
+			});
+		}
+		//else get indexOf 'first'
+		//indexOf first
+		firstIndex = _(models).indexOf(first);
+		//get group of models with identical attribute
+		//and call recursive function on next attribute in the list.
+		models = _(models).filter(function(m){
+			return m.get(attr) === model.get(attr);
+		});
+		attributes = _.last(attributes,attributes.length-1);
+		
+		return firstIndex + this._sortedIndex(model,models,attributes);
+	},
+	
+	/**
+	 * Recursive sort
+	 */
+	_sortBy : function(models,attributes){
+		var attr,
+				that = this;
+		//special case
+		if(!attributes.length){
+			return this.models;
+		}
+		//base case
+		if(attributes.length === 1){
+			attr = attributes[0];
+			return _(models).sortBy(function(model){
+				return model.get(attr);
+			});
+		}
+		else{
+			attr = attributes[0];
+			attributes = _.last(attributes,attributes.length-1);
+			
+			//split up models by sort attribute, 
+			//then call _sortBy with remaining attributes
+			models = _(models).chain().
+				sortBy(function(model){
+					return model.get(attr);
+				}).
+				groupBy(function(model){
+					return model.get(attr);
+				}).
+				toArray().
+				value();
+			
+			_(models).each(function(modelSet,index){
+				models[index] = that._sortBy(models[index],attributes);
+			});
+			return _(models).flatten();	
+		}
+	}
+});
